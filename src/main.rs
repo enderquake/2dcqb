23c99d8953124b16cd02fad2382674d4dda276f7
Krystofire88 <krystofire8888@gmail.com>
2026-06-15 22:22:30 +0200

    basic stucts

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(nonstandard_style)]
 use ::rand::Rng;
+//use ::windows;
 
-fn main() {
-    const VLAN: &str = "Institutionalized discrimination";
-    const DNS: &str = "Always works great for me";
-    const STATIC: &str = "My network never goes down because I use ... routing";
-    const BRNO: &str = "MRDAT";
-    let msg: String = "meow ;3".to_string();
-
-    println!("2dcqb!");
-    let value = rand::thread_rng().gen_range(0..=5);
-
-    while true {
-        println!("{msg}");
-    }
+enum Ammo {
+    a_762,
+    a_300blk,
+    a_556,
+    a_9mm,
+    a_45acp,
+    a_slug,
+    a_buckshot,
+    a_50ae,
+    a_beanbag,
 }
+enum GunType {
+    AssaultRifle,
+    BattleRifle,
+    Shotgun,
+    Pistol,
+    SMG,
+    PDW,
+    Rev,
+    ol,
+    ver}
+struct Gun {
+    name: String,
+    ammo_type: Ammo,
+    mag_size: i32,
+    firerate: i32,
+    gtype: GunType,
+    is_primary: bool,
+}
+enum PickupType {
+    quest,
+    key,
+    ammo,
+    health,
+    attachment,
+    armor,
+    gun,
+}
+struct Pickup {
+    ptype: PickupType,
+    value: i32,
+}
+
+fn main() {}
