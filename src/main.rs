dc5e5c13d7b24ebbcbe4723632f1763e81f400bf
Krystofire88 <krystofire8888@gmail.com>
2026-06-15 22:01:03 +0200

    testing rust llamo
    Co-authored-by: enderquake <lmfao>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use ::rand::Rng;
+
 fn main() {
+    const VLAN: &str = "Institutionalized discrimination";
+    const DNS: &str = "Always works great for me";
+    const STATIC: &str = "My network never goes down because I use ... routing";
+    const BRNO: &str = "MRDAT";
+    let msg: String = "meow ;3".to_string();
+
     println!("2dcqb!");
+    let value = rand::thread_rng().gen_range(0..=5);
+
+    while true {
+        println!("{msg}");
+    }
 }
